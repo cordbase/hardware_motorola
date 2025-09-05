c3bf302ee1a80e4f02728e4e282f5ce8023eba66
CordBase <144811064+cordbase@users.noreply.github.com>
2025-09-05 17:41:15 +0530

    Delete ViPER4AndroidFX directory

--- ViPER4AndroidFX/setup.mk
+++
@@ -1,8 +0,0 @@
-CONFIG_PATH := hardware/motorola/ViPER4AndroidFX
-
-# SEPolicy
-BOARD_VENDOR_SEPOLICY_DIRS += $(CONFIG_PATH)/sepolicy/vendor
-
-PRODUCT_PACKAGES += \
-    ViPER4AndroidFX \
-    libv4a_re
